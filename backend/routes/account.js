e14a9ecda27fe01ea15552c808aabce58eb56fa3
brihat2590 <thapabrihatbir321@gmail.com>
2025-01-19 11:49:55 +0530

    added increment and decrement balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -3,6 +3,7 @@ const app=express()
 const accountRouter=express.Router();
 import { authmiddleware } from "../middleware";
 import { Account } from "../db";
+import { string } from "zod";
 accountRouter.post("/")
 
 accountRouter.get("/balance",authmiddleware,async(req,res)=>{
@@ -14,6 +15,58 @@ accountRouter.get("/balance",authmiddleware,async(req,res)=>{
     })
 
 })
+accountRouter.post("/transfer",authmiddleware,async(req,res)=>{
+    const{amount,to}=req.body
+    const requiredAccountBody=z.object({
+        account:{
+            type:String,
+            required:true,
+
+        },
+        to:{
+            type:String,
+            required:true
+        }
+        
+    })
+    const account=await Account.findOne({
+        userId:req.userId
+    })
+    if(account.balance<amount){
+        res.json({
+            message:"insufficient funds bro"
+        })
+    }
+    const toAccount=await Account.findOne({
+        userId:req.userId
+    })
+    if(!toAccount){
+        res.json({
+            message:"invalid account bro"
+        })
+    }
+    await Account.updateOne({
+        userId: req.userId
+    }, {
+        $inc: {
+            balance: -amount
+        }
+    })
+    await Account.updateOne({
+        userId: to
+    }, {
+        $inc: {
+            balance: amount
+        }
+    })
+
+    res.json({
+        message: "Transfer successful"
+    })
+});
+
+
+
 
 
 export default accountRouter;
