58c0fa248ed9e2ceef42ab125e3673ee255e327c
brihat2590 <thapabrihatbir321@gmail.com>
2025-01-19 11:37:38 +0530

    adde the account routes for balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,7 +1,19 @@
 import express from "express"
 const app=express()
 const accountRouter=express.Router();
+import { authmiddleware } from "../middleware";
+import { Account } from "../db";
 accountRouter.post("/")
+
+accountRouter.get("/balance",authmiddleware,async(req,res)=>{
+    const account=await Account.findOne({
+        userId:req.userId
+    })
+    res.json({
+        balance:account.balance
+    })
+
+})
 
 
 export default accountRouter;
