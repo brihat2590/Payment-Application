017fdb6e1bfe5735c17680b1b82242783b6c7d6d
brihat2590 <thapabrihatbir321@gmail.com>
2025-01-20 21:19:35 +0530

    added the navigation with name and userid

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -1,9 +1,13 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
+
+
 
 export default  function Users(){
     const[users,setUsers]=useState([]);
     const[filter,setFilter]=useState("");
+    
 
     useEffect(()=>{
         axios.get("http://localhost:3000/api/v1/user/bulk?filter="+filter).then(response=>setUsers(response.data.user))
@@ -34,6 +38,7 @@ export default  function Users(){
 }
 
 function User({user}:any) {
+    const navigate=useNavigate()
     return <div className="flex justify-between items-centre 
     ">
         <div className="flex items-center">
@@ -50,7 +55,7 @@ function User({user}:any) {
         </div>
 
         <div className="flex flex-col p-4   ">
-            <button className="p-2 ml-12 rounded-md text-neutral-300  bg-gray-800 cursor-pointer">Send Money</button>
+            <button className="p-2 ml-12 rounded-md text-neutral-300  bg-gray-800 cursor-pointer" onClick={()=>navigate("/send?id="+user._id+"&name="+user.firstName)} >Send Money</button>
         </div>
     </div>
 }
