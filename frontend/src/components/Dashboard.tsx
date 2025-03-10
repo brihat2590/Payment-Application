e535802ae3c0b22024e9eb69115375363706b49d
brihat2590 <thapabrihatbir321@gmail.com>
2025-03-10 21:26:31 +0530

    nav up

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -24,14 +24,29 @@ function Dashboard() {
   return (
     <div className="min-h-screen w-full flex flex-col   ">
         <div className="flex items-center justify-between border-b border-2  ">
-            <div className="p-3 m-2 text-2xl font-semibold bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent  md:text-5xl ml-3">Payments</div>
+            <div className="flex items-center">
+                <svg
+                    className="h-10 w-10 text-blue-600 ml-5"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                >
+                    <path d="M12 0L3 7l1.63 1.27L12 14l7.36-5.73L21 7l-9-7zm0 7.73L5.64 12 12 16.27 18.36 12 12 7.73zM21 9v12l-9-7-9 7V9l1.63 1.27L12 16l7.36-5.73L21 9z" />
+                </svg>
+                
+                <div className=" text-2xl font-semibold bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent  md:text-5xl ml-3 tracking-tighter">PaySwift
+                    
+                </div>
+            </div>
+            
             <div className="flex justify-center items-center m-2 p-3">
                 <div className="pt-3 pr-3 pb-3  m-1 text-xl bg-gradient-to-r from-blue-300 to-blue-600 bg-clip-text text-transparent">Hello</div>
                 <div className="pt-3 pr-3 pb-3  text-lg font-semibold">{<User/>}</div>
                 <div className="rounded-full h-10 w-10 bg-slate-200 flex justify-center mt-1 mr-2 ml-2">
-                    <div className="flex flex-col justify-center h-full text-xl "><FirstLetter/></div>
+                    <div className="flex flex-col justify-center h-full text-xl " onClick={(e)=>{
+                        setOpen(!open)
+                    }}><FirstLetter/></div>
                 </div>
-                <div><Logout/></div>
+                <div>{open&&<div className="absolute right-0 mt-12 w-25 bg-white shadow-lg rounded-lg border  p-2 "><Logout/></div>}</div>
                 
                 
             </div>
--- frontend/src/components/logout.tsx
+++ frontend/src/components/logout.tsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom"
+import { CiSettings } from "react-icons/ci";
+import { TbLogout } from "react-icons/tb";
+
 
 export default function Logout(){
     const navigate=useNavigate()
@@ -10,7 +13,17 @@ export default function Logout(){
     }
     return(
         <div>
-            <button onClick={handlelogout} className="border-2 border-transparent py-3 px-5 m-2 bg-blue-300 rounded-lg w-15 shadow-lg">logout</button>
+            <div onClick={handlelogout} className="w-full px-4 py-2 text-left text-red-700  rounded-md hover:bg-red-200 pl-2 flex gap-1 items-center">
+                <TbLogout/>
+
+                <span>logout</span>
+                
+                </div>
+            <div className="w-full flex px-4 py-2 text-left text-red-700 hover:bg-red-200 rounded-md  gap-1 items-center pl-2">
+                <CiSettings/>
+                
+                <p>Settings</p>
+                </div>
         </div>
     )
 }
